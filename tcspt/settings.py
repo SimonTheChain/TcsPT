5b65f07d113c4fdaebc96a0dad4595477e7e05b1
Simon Lachaîne <simonthechain@gmail.com>
2016-07-31 23:40:16 -0400

    Made the navbar fixed and static Project status is now displayed as a progress bar

--- tcspt/settings.py
+++ tcspt/settings.py
@@ -135,3 +135,13 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/tcspt/static/',
+]
